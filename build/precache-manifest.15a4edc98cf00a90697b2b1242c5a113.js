602cd23fe34d33aefdfbd42f05c550d34532bcef
Daniel Earwicker <dan@earwicker.com>
2019-09-25 14:08:44 +0100

    Include build output

---
+++ build/precache-manifest.15a4edc98cf00a90697b2b1242c5a113.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97fe7ef296fe9ebe3f61545f6417bfb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89d0589cf05d86c45f31",
+    "url": "/static/css/main.509ff67c.chunk.css"
+  },
+  {
+    "revision": "16382860e95f9fb93048",
+    "url": "/static/js/2.9b360d0d.chunk.js"
+  },
+  {
+    "revision": "89d0589cf05d86c45f31",
+    "url": "/static/js/main.1a22374d.chunk.js"
+  },
+  {
+    "revision": "3b0abf8bb451a9a32a2f",
+    "url": "/static/js/runtime-main.4cd110a0.js"
+  }
+]);
