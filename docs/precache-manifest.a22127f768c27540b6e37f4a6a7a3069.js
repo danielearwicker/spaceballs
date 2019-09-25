9b8be0d1b46a658b13db4744bfea8bb9c63f6774
Daniel Earwicker <dan@earwicker.com>
2019-09-25 14:27:29 +0100

    Again

---
+++ docs/precache-manifest.a22127f768c27540b6e37f4a6a7a3069.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f06f2d5c5082a02bd17baf4641b265a",
+    "url": "/spaceballs/index.html"
+  },
+  {
+    "revision": "89d0589cf05d86c45f31",
+    "url": "/spaceballs/static/css/main.509ff67c.chunk.css"
+  },
+  {
+    "revision": "16382860e95f9fb93048",
+    "url": "/spaceballs/static/js/2.9b360d0d.chunk.js"
+  },
+  {
+    "revision": "89d0589cf05d86c45f31",
+    "url": "/spaceballs/static/js/main.1a22374d.chunk.js"
+  },
+  {
+    "revision": "c8a6021d34fccb1538c8",
+    "url": "/spaceballs/static/js/runtime-main.56b61280.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.15a4edc98cf00a90697b2b1242c5a113.js"
+  "/spaceballs/precache-manifest.a22127f768c27540b6e37f4a6a7a3069.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spaceballs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
